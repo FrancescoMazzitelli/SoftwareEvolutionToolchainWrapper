04b630ab51d7441ec6a4822bbb9cd80d611d44c5
FrancescoMazzitelli <pannemar2@gmail.com>
2022-12-16 10:19:32 +0100

    Aggiornamento main

--- main.py
+++ main.py
@@ -41,17 +41,17 @@ if __name__ == '__main__':
     #repo = git_ck.repo_to_use()
     #git_commits = git_ck.get_commits(repo)
     #git_ck.year_ck_metrics(git_commits)
-    #git_ck.date_ck_metrics(git_commits)
+    
     #git_ck.delete_unnecessary(file_to_keep="class")
 
-    analysis.metrics_average()
-    analysis.graph_plot()
+    #analysis.metrics_average()
+    #analysis.graph_plot()
     
     #---------------------------------------------------NICAD-------------------//
-    '''for file in check_nicad_input():
-        xml = minidom.parse(file)
-        nicad.xml_wrapper(xml)
-'''
+    #for file in check_nicad_input():
+    #    xml = minidom.parse(file)
+    #    nicad.xml_wrapper(file, xml)
+
     #-------------------------------------------------SONARQUBE-----------------//
     #sonar.sonar()
     
